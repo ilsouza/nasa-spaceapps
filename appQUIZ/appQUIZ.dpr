4c7704709e77942bca00a5b94da00fe82a7eeb10
ilsouza <ilsouza@gmail.com>
2020-10-05 22:29:05 -0300

    adjusts main
    adjusts main

--- appQUIZ/appQUIZ.dpr
+++ appQUIZ/appQUIZ.dpr
@@ -3,18 +3,18 @@ program appQUIZ;
 uses
   System.StartUpCopy,
   FMX.Forms,
-  FMain in 'src\FMain.pas' {frmMain},
-  FQuiz in 'src\FQuiz.pas' {frmQuiz},
-  FPlayer in 'src\FPlayer.pas' {frmPlayer},
-  UDM in 'src\UDM.pas' {dm: TDataModule};
+  UDM in 'src\services\UDM.pas' {dm: TDataModule},
+  FMain in 'src\views\FMain.pas' {frmMain},
+  FPlayer in 'src\views\FPlayer.pas' {frmPlayer},
+  FQuiz in 'src\views\FQuiz.pas' {frmQuiz};
 
 {$R *.res}
 
 begin
   Application.Initialize;
-  Application.CreateForm(TfrmMain, frmMain);
-  Application.CreateForm(TfrmQuiz, frmQuiz);
-  Application.CreateForm(TfrmPlayer, frmPlayer);
   Application.CreateForm(Tdm, dm);
+  Application.CreateForm(TfrmMain, frmMain);
+  Application.CreateForm(TfrmPlayer, frmPlayer);
+  Application.CreateForm(TfrmQuiz, frmQuiz);
   Application.Run;
 end.
--- serverQUIZ/src/services/uDMQuizActions.pas
+++ serverQUIZ/src/services/uDMQuizActions.pas
@@ -3,20 +3,14 @@ unit uDMQuizActions;
 interface
 
 uses
-  System.SysUtils, System.Classes, FireDAC.Stan.Intf, FireDAC.Stan.Option, FireDAC.Stan.Param, FireDAC.Stan.Error,
-  FireDAC.DatS, FireDAC.Phys.Intf, FireDAC.DApt.Intf, FireDAC.Stan.Async, FireDAC.DApt, Data.DB, FireDAC.Comp.DataSet,
-  FireDAC.Comp.Client,
-  UDM;
+  System.SysUtils, System.Classes, FireDAC.Stan.Intf, FireDAC.Stan.Option, FireDAC.Stan.Param, FireDAC.Stan.Error, FireDAC.DatS, FireDAC.Phys.Intf, FireDAC.DApt.Intf, FireDAC.Stan.Async, FireDAC.DApt, Data.DB, FireDAC.Comp.DataSet, FireDAC.Comp.Client;
 
 type
   TdmQuizActions = class(TDataModule)
     qryQuizActionGetItem: TFDQuery;
     qryQuizActionPostItem: TFDQuery;
-    procedure DataModuleCreate(Sender: TObject);
-    procedure DataModuleDestroy(Sender: TObject);
   private
     { Private declarations }
-    FDM : Tdm;
   public
     { Public declarations }
   end;
@@ -28,25 +22,8 @@ implementation
 
 {%CLASSGROUP 'System.Classes.TPersistent'}
 
+uses uDM;
+
 {$R *.dfm}
-
-procedure TdmQuizActions.DataModuleCreate(Sender: TObject);
-begin
-  if (FDM = nil) then
-  begin
-    FDM := Tdm.Create(nil);
-  end;
-
-  qryQuizActionGetItem.Connection  := FDM.conQuiz;
-  qryQuizActionPostItem.Connection := FDM.conQuiz;
-end;
-
-procedure TdmQuizActions.DataModuleDestroy(Sender: TObject);
-begin
-  if Assigned(FDM) then
-  begin
-    FDM.DisposeOf;
-  end;
-end;
 
 end.
