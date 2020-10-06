058f67fab896061ac4c71fa26804670f9ef5e2cb
Ivan Luciano de Souza <ilsouza@gmail.com>
2020-10-05 21:39:57 -0300

    Add files via upload

---
+++ appQUIZ/src/views/FPlayer.pas
@@ -0,0 +1,44 @@
+unit FPlayer;
+
+interface
+
+uses
+  System.SysUtils, System.Types, System.UITypes, System.Classes, System.Variants,
+  FMX.Types, FMX.Controls, FMX.Forms, FMX.Graphics, FMX.Dialogs, FMX.Controls.Presentation, FMX.StdCtrls,
+  FMX.Objects, FMX.Layouts, FMX.WebBrowser;
+
+type
+  TfrmPlayer = class(TForm)
+    Layout1: TLayout;
+    Layout2: TLayout;
+    Layout3: TLayout;
+    btnBack: TButton;
+    btnPlayList: TButton;
+    webBrowser: TWebBrowser;
+    procedure btnPlayListClick(Sender: TObject);
+    procedure btnBackClick(Sender: TObject);
+  private
+    { Private declarations }
+  public
+    { Public declarations }
+  end;
+
+var
+  frmPlayer: TfrmPlayer;
+
+implementation
+
+{$R *.fmx}
+
+procedure TfrmPlayer.btnBackClick(Sender: TObject);
+begin
+  frmPlayer.webBrowser.Navigate('https://www.nasa.gov/content/nasa-history/videos');
+  Close;
+end;
+
+procedure TfrmPlayer.btnPlayListClick(Sender: TObject);
+begin
+  ShowMessage('adicionado à playList do youtube');
+end;
+
+end.
